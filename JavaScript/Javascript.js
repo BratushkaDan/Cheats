d550919eea019e8d6ffb931f5babd5e0a4849c37
Aszura <40485301+BratushkaDan@users.noreply.github.com>
2021-04-22 23:01:10 +0300

    Update Javascript.js

--- JavaScript/Javascript.js
+++ JavaScript/Javascript.js
@@ -220,64 +220,6 @@ let obj = { a: 1 },
 let sym = Symbol("Hello, World!"); // no 'new' keyword for construction
 obj[sym] = 123;
 
-//----- Iterator
-let array = [1, 2, 3];
-let it = array[Symbol.iterator]();
-it.next();
-
-//----- Generators
-function *generator() {
-    let arr = [ yield 1, yield 2, yield 3];
-}
-const generator2 = function* (x) {};
-function *additional() {
-    yield *[1,2,3];
-}
-for (let v of additional()) {
-    console.log(v);
-}
-
-// ----- Async functions
-async function fn() {}
-const fun = async function () {};
-const f = async () => {};
-
-// ----- Async generator functions
-async function* g() {}
-const ag = async function* () {}
-
-//----- User Iterator
-const Fib = {
-    [Symbol.iterator]: () => {
-        let n1 = 1, n2 = 1, current;
-        return {
-            [Symbol.iterator]() {
-                return this;
-            },
-            next() {
-                current = n2;
-                n2 = n1;
-                n1 = n1 + current;
-                if (current > 150) {
-                    console.log("Over.");
-                    return { value: current, done: true};
-                }
-                return { value: current, done: false};
-            },
-            return(v) {
-                console.log("Over.");
-                return { value: v, done: true };
-            }
-        }
-    }
-};
-
-// ----- Promises
-const promise = new Promise((res, rej) => {});
-promise
-  .then(result => {}, reject => {})
-  .catch(err => {});
-promise.catch(err => {});
 // -- Paralell use of Promises
 Promise.all(promise, promise2)
   .then(result => console.log(result)); /* result is array of promises' result */
@@ -287,72 +229,6 @@ Promise.race(promise, promise2)
 // ----- blur(), focus();
 node.focus();
 node.blur();
-
-// ----- Labels
-/*Labels can be assigned to for loops or blocks of code and used with break, continue keywords  */
-
-// ----- Regex
-//Flags
-/*i - regex isn’t case sensitive, g - all matches, m - multiline mode.*/
-String.prototype.search(reg) /*— searches for first match or return -1.*/
-String.prototype.match(reg) /*— without g: returns result, result[0] - first match, result[n] - other matches (if reg grouped by parentheses), result.index - position of match, result.input - str;
-— with g: returns array of matches, null if there’s no matches. */
-String.prototype.split(reg|substring, limit) /* — breaks str into an array using reg, can limit up amount of items.*/
-String.prototype.replace(reg, String.prototype|Function.prototype) /*— replaces all the matches of reg, argument can use: $n - inserts n, $& - inserts all the matches, $` - inserts str part before of match, $’ - inserts str part after the match, func cat take the following ars: func(str, [,p1,p2,..], offset, s) — str - found match, [,p1,p2…] - parentheses content(if they’re present), offset - position where match was found, s - initial string.*/
-regex.test(str) /*— returns true/false if there’s a match*/
-regex.exec(str) /*— searches for matches: regex.lastIndex shows where the search is carrying out.
-Classes and special symbol*/
-/*
-\d - digit, symbol from 0 to 9, \D - not a digit,
-\s - space/tab|str-break, \S - not a space
-\w - word, \w - not a word,
-\b - border of word, \B - not a border
-Spaces are common symbols.
-“.” - any symbol but a break.
-To escape symbol as regular backslash should be used
-Sets and ranges.
-[bac] - set means that any of a, b or c can present.
-[0-5] - range means that any number between 0 and 5 inclusively and present.
-[\wа-яё] - range finds any english and russian letter.
-Exclusion ranges: [^...]
-Symbols inside of sets and ranges are not necessary to escape.
-Quantifiers 
-{ n } — exact quantity; { n, n + m } — quantity from-to; { n, } — quantity from n to infinity;
-+ — one or more, ? —  zero or one, * — zero or more
-Greedy search is the regular one, to switch to lazy search use ? after quantifiers: +?, *?, ??
-( ) — parentheses are used to group up elements.
-for instance, (( )\s*( )) — will store all (3) parentheses in result array, starting from exterior ones.
-To use grouping without storing data inside of parentheses use “?:” g.e.:  (?: )
-\n — used to repeat parentheses in regex, | — alternation.
-^ — at the regex beginning is the text beginning,
-^ — at the regex with m flag is the string beginning,
-$ — at the end of regex defines text ending, 
-$ — at the end of regex with m flag is the string ending.
-*/
-
-// ----- AJAX
-const url = 'https://jsonplaceholder.typicode.com/users/10';
-
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET', url);
-xhr.responseType = 'json';
-xhr.setRequestHeader('Content-Type', 'application/json');
-
-xhr.onreadystatechange = () => {
-    if (xhr.status === 200 && xhr.response) {
-        console.log(xhr.response);
-    }
-    if (xhr.status === 404) {
-        console.log({status: 404, response: xhr.response});
-    }
-};
-// xhr.onload = () => {};
-xhr.onerror = () => {
-    console.log(xhr.response);
-};
-
-xhr.send(/*(JSON.stringify(data) - for POST*/);
 
 // -- Timeouts and Intervals
 this.timerId = setInteval(() => {}, 1000);
